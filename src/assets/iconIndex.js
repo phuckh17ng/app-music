9f8bc1dc84e133bdab3fb657726463daf0d8daae
phuckh17ng <lemauphuckhang171001@gmail.com>
2023-08-25 22:02:22 +0700

    deploy

--- src/assets/iconIndex.js
+++ src/assets/iconIndex.js
@@ -1,81 +1,81 @@
 export const musicPlayIcon = (style) => (
-  <svg
-    class={`${style} w-6 h-6 dark:text-white`}
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    viewBox="0 0 16 16"
-  >
-    <path d="M0 .984v14.032a1 1 0 0 0 1.506.845l12.006-7.016a.974.974 0 0 0 0-1.69L1.506.139A1 1 0 0 0 0 .984Z" />
-  </svg>
+	<svg
+		class={`${style} w-6 h-6`}
+		aria-hidden="true"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="currentColor"
+		viewBox="0 0 16 16"
+	>
+		<path d="M0 .984v14.032a1 1 0 0 0 1.506.845l12.006-7.016a.974.974 0 0 0 0-1.69L1.506.139A1 1 0 0 0 0 .984Z" />
+	</svg>
 );
 
 export const dropDownIcon = (style) => (
-  <svg
-    class={`${style} w-6 h-6 dark:text-white`}
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 14 8"
-  >
-    <path
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2.5"
-      d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
-    />
-  </svg>
+	<svg
+		class={`${style} w-6 h-6`}
+		aria-hidden="true"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 14 8"
+	>
+		<path
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2.5"
+			d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+		/>
+	</svg>
 );
 
 export const musicPauseIcon = (style) => (
-  <svg
-    class={`${style} w-6 h-6`}
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    viewBox="0 0 10 16"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M0 .8C0 .358.32 0 .714 0h1.429c.394 0 .714.358.714.8v14.4c0 .442-.32.8-.714.8H.714a.678.678 0 0 1-.505-.234A.851.851 0 0 1 0 15.2V.8Zm7.143 0c0-.442.32-.8.714-.8h1.429c.19 0 .37.084.505.234.134.15.209.354.209.566v14.4c0 .442-.32.8-.714.8H7.857c-.394 0-.714-.358-.714-.8V.8Z"
-      clip-rule="evenodd"
-    />
-  </svg>
+	<svg
+		class={`${style} w-6 h-6`}
+		aria-hidden="true"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="currentColor"
+		viewBox="0 0 10 16"
+	>
+		<path
+			fill-rule="evenodd"
+			d="M0 .8C0 .358.32 0 .714 0h1.429c.394 0 .714.358.714.8v14.4c0 .442-.32.8-.714.8H.714a.678.678 0 0 1-.505-.234A.851.851 0 0 1 0 15.2V.8Zm7.143 0c0-.442.32-.8.714-.8h1.429c.19 0 .37.084.505.234.134.15.209.354.209.566v14.4c0 .442-.32.8-.714.8H7.857c-.394 0-.714-.358-.714-.8V.8Z"
+			clip-rule="evenodd"
+		/>
+	</svg>
 );
 
 export const musicNextIcon = (style) => (
-  <svg
-    class={style}
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 12 10"
-  >
-    <path
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
-      d="m7 9 4-4-4-4M1 9l4-4-4-4"
-    />
-  </svg>
+	<svg
+		class={style}
+		aria-hidden="true"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 12 10"
+	>
+		<path
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="m7 9 4-4-4-4M1 9l4-4-4-4"
+		/>
+	</svg>
 );
 
 export const musicPrevIcon = (style) => (
-  <svg
-    class={style}
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 12 10"
-  >
-    <path
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
-      d="M5 1 1 5l4 4m6-8L7 5l4 4"
-    />
-  </svg>
+	<svg
+		class={style}
+		aria-hidden="true"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 12 10"
+	>
+		<path
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M5 1 1 5l4 4m6-8L7 5l4 4"
+		/>
+	</svg>
 );
